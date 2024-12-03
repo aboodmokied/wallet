95478b984de2cfead723637006791048b04f985b
AboodMokied <78992620+AboodMokied@users.noreply.github.com>
2024-12-03 15:54:24 +0200

    add user instance in response of login

--- controllers/api/authController.js
+++ controllers/api/authController.js
@@ -9,14 +9,13 @@ exports.register=tryCatch(async(req,res,next)=>{
     // Before: guard and user data validation required, check if user exist.
     const {guard}=req.body;
     const user=await new Register().withGuard(guard).create(req);
-    console.log({user});
     res.status(201).send({status:true,result:{user}});
 });
 
 exports.login=tryCatch(async(req,res,next)=>{
     const {guard}=req.body;
-    const token=await new ApiAuth().withGuard(guard).generateToken(req);
-    res.send({status:true,result:{token}})
+    const {token,user}=await new ApiAuth().withGuard(guard).generateToken(req);
+    res.send({status:true,result:{token,user}});
 })
 
 exports.logout=tryCatch(async(req,res,next)=>{
